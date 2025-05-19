c4ffba270338d7615a3de68b839704784697e389
UmidjonPayme <umidjon.f@payme.uz>
2025-05-19 12:37:48 +0500

    Added XCTest

--- Tests/SPMForRPNCalculatorTests/SPMForRPNCalculatorTests.swift
+++ Tests/SPMForRPNCalculatorTests/SPMForRPNCalculatorTests.swift
@@ -1,6 +1,32 @@
-import Testing
-@testable import SPMForRPNCalculator
+import XCTest
+@testable import RPNImplementation
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class RPNFunctionsTests: XCTestCase {
+
+    func testParseToRPN_simpleExpression() {
+        let tokens = ["3", "+", "4", "*", "2"]
+        let rpn = RPNFunctions.parseToRPN(to: tokens)
+        XCTAssertEqual(rpn, ["3", "4", "2", "*", "+"])
+    }
+
+    func testCalculateRPN_simpleExpression() {
+        let postfix = ["3", "4", "2", "*", "+"]
+        let result = RPNFunctions.calculateRPN(to: postfix)
+        XCTAssertEqual(result, "11")
+    }
+
+    func testDivisionByZero() {
+        let postfix = ["0", "0", "÷"]
+        let result = RPNFunctions.calculateRPN(to: postfix)
+        XCTAssertEqual(result, "Бесконечность")
+    }
+
+    func testUnaryMinus() {
+        let tokens = ["-", "3", "+", "5"]
+        let rpn = RPNFunctions.parseToRPN(to: tokens)
+        XCTAssertEqual(rpn, ["-3", "5", "+"])
+        let result = RPNFunctions.calculateRPN(to: rpn)
+        XCTAssertEqual(result, "2")
+    }
 }
+
